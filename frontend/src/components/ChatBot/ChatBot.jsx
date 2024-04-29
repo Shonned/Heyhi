4aa814f4a12ec76a1021bc9b59c93da374a836fc
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-29 10:44:09 +0100

    added options

--- frontend/src/components/ChatBot/ChatBot.jsx
+++ frontend/src/components/ChatBot/ChatBot.jsx
@@ -6,30 +6,26 @@ import SelectAssistant from "./SelectAssistant/SelectAssistant.jsx";
 
 const ChatBot = () => {
 
-    const [requestTerm, setRequestTerm] = useState('');
-
-    const handleRequestChange = (event) => {
-        setRequestTerm(event.target.value);
-    };
-
     const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     const smallText = "Lorem ipsum dolor sit amet";
+
+    const startOptions = ['Yes', 'No'];
 
     return (
         <div className="chatbot">
             <div className="chatbot-content">
-                <SelectAssistant />
+                <SelectAssistant/>
                 <div className="chatbot-messages">
-                    <MessageBot content={longText} />
-                    <MessageUser content={smallText} />
+                    <MessageBot content="Bonjour" />
+                    <MessageBot content={longText} options={startOptions} />
+                    <MessageUser content={smallText}/>
                 </div>
-                <div className="chatbot-form">
-                    <Input
-                        type="text"
-                        placeholder="Aa"
-                        onChange={handleRequestChange}
-                    />
-                </div>
+            </div>
+            <div className="chatbot-form">
+                <Input
+                    type="text"
+                    placeholder="Aa"
+                />
             </div>
         </div>
     )
--- frontend/src/components/ChatBot/Message/Message.jsx
+++ frontend/src/components/ChatBot/Message/Message.jsx
@@ -4,20 +4,33 @@ const getContentClassName = (content) => {
     return content.length <= 50 ? 'content short-content' : 'content';
 };
 
-const MessageBot = ({ content }) => {
+const MessageBot = ({ content, options }) => {
     return (
         <div className="message message-bot">
             <div className="user">Bot</div>
-            <div className={getContentClassName(content)}>{content}</div>
+            <div className="content-flex">
+                <div className={getContentClassName(content)}>{content}</div>
+                {options && (
+                    <div className="options">
+                        {options.map((option, index) => (
+                            <div key={index} className="option">
+                                <span>{option}</span>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
 
-const MessageUser = ({ content }) => {
+const MessageUser = ({ content, options }) => {
     return (
         <div className="message message-user">
             <div className="user">You</div>
-            <div className={getContentClassName(content)}>{content}</div>
+            <div className="content-flex">
+                <div className={getContentClassName(content)}>{content}</div>
+            </div>
         </div>
     );
 };
