6fffcea6ae6fe103cead93838f88467884024f07
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-29 10:21:35 +0100

    better display of messages

--- frontend/src/components/ChatBot/ChatBot.jsx
+++ frontend/src/components/ChatBot/ChatBot.jsx
@@ -1,7 +1,7 @@
 import './ChatBot.css'
 import Input from "../Form/Input/Input.jsx";
 import {useState} from "react";
-import Message from "./Message/Message.jsx";
+import { MessageBot, MessageUser } from './Message/Message.jsx';
 import SelectAssistant from "./SelectAssistant/SelectAssistant.jsx";
 
 const ChatBot = () => {
@@ -12,12 +12,16 @@ const ChatBot = () => {
         setRequestTerm(event.target.value);
     };
 
+    const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    const smallText = "Lorem ipsum dolor sit amet";
+
     return (
         <div className="chatbot">
             <div className="chatbot-content">
                 <SelectAssistant />
                 <div className="chatbot-messages">
-                    <Message />
+                    <MessageBot content={longText} />
+                    <MessageUser content={smallText} />
                 </div>
                 <div className="chatbot-form">
                     <Input
--- frontend/src/components/ChatBot/Message/Message.jsx
+++ frontend/src/components/ChatBot/Message/Message.jsx
@@ -1,18 +1,25 @@
 import './Message.css'
-import {useState} from "react";
 
-const Message = () => {
+const getContentClassName = (content) => {
+    return content.length <= 50 ? 'content short-content' : 'content';
+};
 
+const MessageBot = ({ content }) => {
     return (
-        <div className="message">
-            <div className="user">
-
-            </div>
-            <div className="content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consequatur cumque cupiditate deleniti dicta eum explicabo illum ipsa ipsam, officia pariatur quisquam quod reprehenderit saepe soluta. A atque dolores itaque!
-            </div>
+        <div className="message message-bot">
+            <div className="user">Bot</div>
+            <div className={getContentClassName(content)}>{content}</div>
         </div>
-    )
-}
+    );
+};
 
-export default Message;
+const MessageUser = ({ content }) => {
+    return (
+        <div className="message message-user">
+            <div className="user">You</div>
+            <div className={getContentClassName(content)}>{content}</div>
+        </div>
+    );
+};
+
+export { MessageBot, MessageUser };
