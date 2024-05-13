be482e647ac93ce7ce2b7e624eeece738509fd32
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-05-13 12:46:36 +0100

    sort history by last message

--- backend/api/message/routes.py
+++ backend/api/message/routes.py
@@ -29,5 +29,10 @@ async def create_message(conv_uid: str, content: str):
     doc_ref_user_tuple = db.collection(u'messages').add(user_data)
     doc_ref_bot_tuple = db.collection(u'messages').add(bot_data)
 
+    conversation_ref = db.collection(u'conversations').document(conv_uid)
+    conversation_data = conversation_ref.get().to_dict()
+    conversation_data["updated_at"] = datetime.utcnow()
+    conversation_ref.set(conversation_data)
+
     return JSONResponse(content={"user_message": content, "bot_response": bot_response}, status_code=201,
                         headers={"Access-Control-Allow-Origin": "*"})
